52f1c0a2332a42736606adfb11ddcfed292ffe6c
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-13 14:05:29 -0300

    Update main.py

--- teste01-pytube/main.py
+++ teste01-pytube/main.py
@@ -1,73 +1,31 @@
 import os
-# import tkinter as tk
+import tkinter as tk
 from time import sleep
 from pytube import YouTube
 
-while True: 
-    try: # Adiciona a URl youtube a class Youtube
-        link_youtube = str(input('Link: '))
-        yt = YouTube(link_youtube)
-    except BaseException as error:
-        print(f'Ocorreu um erro {error}')
-    else:
-        break
+LINK_ = 'https://www.youtube.com/shorts/wS5guAl4T64'
 
-def title_(text: str) -> None:
-    print('==='*20)
-    print(text)
-    print('==='*20)
+class App:
+    def __init__(self, link_youtube: str = None):
+        # Cria a janela principal e executa loop
+        self.root = tk.Tk()
+        self.title = self.root.title('Youtube vídeo downloader')
+        self.root.mainloop()
 
+        while True: 
+            try: # Adiciona a URl youtube a class Youtube
+                self._yt = YouTube(link_youtube)
+            except BaseException as error:
+                print(f'Ocorreu um erro {error}')
+            else:
+                break
+        
+    def download_video(self):
+        pass
 
-def downloading_video(streams_: yt) -> None:
-    os.system('cls')
-    print('carregando...') # retorna todas as qualidades disponiveis no vídeo
-    list_quality_video = streams_.streams.filter(file_extension='mp4').order_by('resolution')
-    title_('Escolha a qualidade do vídeo!')
-    for numbers, qualitys in enumerate(list_quality_video):
-        print(f'Número {numbers+1} Qualidade {qualitys}')
-    try:
-        # Escolha de resolução mais o download da mesma escolhida
-        choice_resolution = str(input('qualidade do video resolução disponivel: '))
-        print('Aguarde.... o download ira ser feito')
-        stream_download = streams_.streams.get_by_resolution(choice_resolution)
-        stream_download.download()
-        print('Download concluido!')
-    except BaseException as erro:
-        os.system('cls')
-        print(f'Ocorreu um erro por favor tente de novo erro {erro}')
-        sleep(2)
-
-
-def downloading_audio(audio_: yt) -> None:
-    os.system('cls')
-    print('O audio que sera baixado e o de melhor qualidade mp3 disponivel do vídeo')
-    # audio_download coletara a melhor qualidade possivel de audio e fara o download
-    audio_download = audio_.streams.filter(only_audio=True).first()
-    out_file_download = audio_download.download()
-    # name_base e o nome base do arquivo o out_point e o formato depois do ponto .mp3, .mp4 etc..
-    name_base, out_point = os.path.splitext(out_file_download)
-    # remove formato anterior adiciona o formato mp3
-    new_file_download = name_base + '.mp3'
-    os.rename(out_file_download, new_file_download)
-    print('Download concluido...')
-    sleep(2)
-
+    def download_audio(self):
+        pass
 
 if __name__ == '__main__':
-    while True:
-        title_(f'Titulo do vídeo --> {yt.title}')
-        print("Você Deseja baixar o Vídeo ou o Audio digite 'V' ou 'A'")
-        question = str(input('Video [V] | Audio [A] | Sair [S]: ')).upper()[0]
-        if 'V' in question:
-            downloading_video(yt)
-        elif 'A' in question:
-            downloading_audio(yt)
-        elif 'S' in question:
-            os.system('cls')
-            print('saindo do programa...')
-            break
-        else:
-            os.system('cls')
-            print('Por favor tente novamente! o caractere digitado não existe')
-            sleep(2)
-            continue
+    # app = App(input('link youtube: '))
+    App(LINK_)
