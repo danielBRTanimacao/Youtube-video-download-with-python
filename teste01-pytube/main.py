c2de0dc45e31fba1e54aa3830b07924c4841e930
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-09-14 15:26:26 -0300

    modificando alguns indices e variaveis

--- teste01-pytube/main.py
+++ teste01-pytube/main.py
@@ -5,13 +5,8 @@ from pytube import YouTube
 
 LINK_ = 'https://www.youtube.com/shorts/wS5guAl4T64'
 
-class App:
-    def __init__(self, link_youtube: str = None):
-        # Cria a janela principal e executa loop
-        self.root = tk.Tk()
-        self.title = self.root.title('Youtube vídeo downloader')
-        self.root.mainloop()
-
+class Aplication:
+    def __init__(self, link_youtube: str = None) -> None:
         while True: 
             try: # Adiciona a URl youtube a class Youtube
                 self._yt = YouTube(link_youtube)
@@ -19,13 +14,31 @@ class App:
                 print(f'Ocorreu um erro {error}')
             else:
                 break
-        
-    def download_video(self):
+        # Cria a janela principal
+        self.root = tk.Tk()
+        self._screen()
+        self._title_(f'Titulo do vídeo: ---> {self._yt.title} <---')
+        # executa loop
+        self.root.mainloop()
+    
+    def _screen(self) -> None:
+        self.title = self.root.title('Youtube vídeo downloader')
+        self.root.geometry('700x500')
+
+    def _title_(self, text_: str) -> None:
+        label = tk.Label(self.root, text=text_)
+        label.pack(pady=20, padx=20)
+        # print('==='*20)
+        # print(text)
+        # print('==='*20)    
+    
+    def _download_video(self, streams):
         pass
 
-    def download_audio(self):
+    def _download_audio(self):
         pass
+
 
 if __name__ == '__main__':
-    # app = App(input('link youtube: '))
-    App(LINK_)
+    # App(input('link youtube: '))
+    Aplication(LINK_)
