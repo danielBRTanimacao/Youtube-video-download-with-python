12384a7e634b325d0950ac65c6a840bcb4aa9ee6
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-16 17:14:18 -0300

    removing backend

--- backend/video_downloader/static/js/scripts.js
+++
@@ -1,5 +0,0 @@
-var title_video = window.document.getElementById('titulo')
-
-function sendTitle() {
-    title_video.innerText = "Sem titulo (teste)"
-}
--- backend/video_downloader/video_downloader/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from app_downloader import views
-
-urlpatterns = [
-    path('', views.home, name='home')
-    # path('forms/')
-]
