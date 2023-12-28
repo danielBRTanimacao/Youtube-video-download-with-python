809d0abc7073dad6abddc14b59947fc927e90457
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2023-12-28 10:10:28 -0300

    Update Download section and UX prerender and functions init

--- frontend/src/components/DownloadArea.jsx
+++ frontend/src/components/DownloadArea.jsx
@@ -1,25 +1,47 @@
+import { useState } from "react";
+
 import VideoDownloads from "./VideoDownloads.jsx";
 
 export default () => {
-    const link = "https://www.youtube.com/embed/zpOULjyy-n8?rel=0";
+    const [linkTxt, setLink] = useState(
+        ""
+        // "https://www.youtube.com/embed/zpOULjyy-n8?rel=0"
+    );
+
+    function cathLink() {
+        let txtlink = document.querySelector("#linkCather").value;
+        setLink(txtlink);
+    }
 
     return (
         <>
-            <section className="p-5 container text-center">
-                <div>
-                    <h1>Insira um link abaixo para download</h1>
-                </div>
-                <div>
-                    <div className="input-group pt-5 pb-3">
-                        <input type="text" className="form-control" />
-                        <button className="btn btn-primary">enviar</button>
+            <section className="bg-yt">
+                <div className="p-5 container text-center">
+                    <div className="d-flex justify-content-center">
+                        <h1 className="me-2 pt-1">Insira um link e baixe</h1>
+                        <h1 className="display-4 fs-bowb color-primary txt-deco no-under">
+                            qualquer tipo de vídeo!
+                        </h1>
                     </div>
-                    <div>
-                        <p>pequeno texto</p>
+                    <div className=" row">
+                        <div className="pt-5 pb-3">
+                            <input
+                                type="text"
+                                className="form-control form-control-lg col-md-6 shadow"
+                                placeholder="link..."
+                                id="linkCather"
+                            />
+                            <button
+                                className="btn btn-lg btn-primary col-md-6 mt-3"
+                                onClick={cathLink}
+                            >
+                                enviar
+                            </button>
+                        </div>
                     </div>
                 </div>
             </section>
-            <VideoDownloads link={link} />
+            <VideoDownloads link={linkTxt} />
         </>
     );
 };
--- frontend/src/components/VideoDownloads.jsx
+++ frontend/src/components/VideoDownloads.jsx
@@ -1,15 +1,23 @@
-export default (linkprop) => {
-    return (
-        <>
-            <section>
-                <h1 className="pb-5">
-                    downloads tips so aparece quando clicar kkk
-                </h1>
-                <div className="ratio ratio-16x9 container w-50">
-                    <iframe src={linkprop.link} allowFullScreen></iframe>
-                </div>
-                <div className="pt-5">links tipe download</div>
-            </section>
-        </>
-    );
+export default (linkprop, validprop) => {
+    if (validprop) {
+        return (
+            <>
+                <section>
+                    <h1 className="pb-5">
+                        downloads tips so aparece quando clicar kkk
+                    </h1>
+                    <div className="ratio ratio-16x9 container w-50">
+                        <iframe src={linkprop.link} allowFullScreen></iframe>
+                    </div>
+                    <div className="pt-5">links tipe download</div>
+                </section>
+            </>
+        );
+    } else {
+        return (
+            <>
+                <h1>Deu ruim</h1>
+            </>
+        );
+    }
 };
