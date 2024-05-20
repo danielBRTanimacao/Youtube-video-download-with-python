a46dc51625c903a3c84c8a5a7637364d47ffc5ee
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-20 14:44:50 -0300

    add pytube and making the logical to download videos

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -1,7 +1,11 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from pytube import YouTube
 
 home_route = Blueprint('home', __name__)
 
-@home_route.route('/')
+@home_route.route('/', methods=['POST', 'GET'])
 def home():
+    if request.method == "POST":
+        q = request.form['q']
+        yt = YouTube(q)
     return render_template('index.html')
