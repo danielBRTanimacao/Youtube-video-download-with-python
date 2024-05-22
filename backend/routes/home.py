49e6a05ba83293d72f8c90b47da44e4b5fb5a440
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-22 11:48:44 -0300

    init of function and process to find propertis download

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -7,7 +7,12 @@ home_route = Blueprint('home', __name__)
 def home():
     if request.method == "POST":
         q = request.form['q']
-        yt = YouTube(q)
-        list_ofv = yt.streams.filter(file_extension='mp4', progressive="False")
-        return render_template('index.html', list_videos=list_ofv)
+        try:
+            yt = YouTube(q)
+            title_video = yt.title
+            url_thumb = yt.thumbnail_url
+            list_ofv = yt.streams.filter(file_extension='mp4', progressive=False)
+            return render_template('index.html', list_videos=list_ofv, title_video=title_video, url_thumb=url_thumb)
+        except:
+            return render_template('index.html', error="URL invalida!")
     return render_template('index.html')
