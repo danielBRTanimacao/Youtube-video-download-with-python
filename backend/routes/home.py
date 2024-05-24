a69561facecb4523bf983c411baee96e5f09419d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-05-24 14:37:57 -0300

    tests to download

--- backend/routes/home.py
+++ backend/routes/home.py
@@ -13,7 +13,7 @@ def home():
             title_video = yt.title
             url_thumb = yt.thumbnail_url
             list_videos = yt.streams
-            return render_template('index.html', list_videos=list_videos, title_video=title_video, url_thumb=url_thumb)
+            return render_template('index.html', list_videos=list_videos, title_video=title_video, url_thumb=url_thumb, url=q)
         except RegexMatchError:
             return render_template('index.html', error="Erro: URL invalida!")
         except AttributeError:
@@ -25,9 +25,11 @@ def home():
     return render_template('index.html')
 
 @home_route.route('/download', methods=['POST'])
-def download(link = ""):
-    print(link)
+def download():
     r = request.form['r']
-    print(r)
+    v = request.form['v']
+    yt = YouTube(v)
+    stream = yt.streams.get_by_itag(r)
+    stream.download()
     return render_template('index.html')
 # tentar salvar o link aqui para baixar
