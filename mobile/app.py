88029cfb166a98952d52a4405836a2a1d05ac72d
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-12 11:26:05 -0300

    simple tests

--- mobile/app.py
+++ mobile/app.py
@@ -1,19 +1,21 @@
 import flet as ft
 
+from utils.validators import url_validator
+
 def main(page: ft.Page):
     page.title = "Youtube vídeo downloader"
     
     def submit_link(e):
-        if not txt_name.value:
-            txt_name.error_text = "Cole aqui uma URL valida!"
+        if not url_input.value or not url_validator(url_input.value):
+            url_input.error_text = "Cole aqui uma URL valida!"
             page.update()
         else:
-            link_video = txt_name.value
+            link_video = url_input.value
             page.clean()
             page.add(ft.Text(f"Link do video: {link_video}"))
 
-    txt_name = ft.TextField(label="URL youtube")
+    url_input = ft.TextField(label="URL youtube")
 
-    page.add(txt_name, ft.ElevatedButton("Enviar", on_click=submit_link))
+    page.add(url_input, ft.ElevatedButton("Enviar", on_click=submit_link))
 
 ft.app(target=main)
