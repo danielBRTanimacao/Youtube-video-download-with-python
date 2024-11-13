4f9e007af526da2f6a930799a466537cac9c8421
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-13 09:08:47 -0300

    new feature mobile design

--- mobile/app.py
+++ mobile/app.py
@@ -3,8 +3,18 @@ import flet as ft
 from utils.validators import url_validator
 
 def main(page: ft.Page):
-    page.title = "Youtube vídeo downloader"
-    title_principal = ft.Text(value="Youtube vídeo downloader")
+    page.title = "Python vídeo downloader"
+    title_principal = ft.Row(
+        [
+            ft.Icon(ft.icons.PLAY_ARROW, size=35, color="red"),
+            ft.Text(
+                "Youtube vídeo downloader",
+                size=30,
+                weight=ft.FontWeight.BOLD,
+            ),
+        ],
+        alignment=ft.MainAxisAlignment.CENTER,
+    )
     
     def submit_link(e):
         if not url_input.value or not url_validator(url_input.value):
@@ -13,7 +23,9 @@ def main(page: ft.Page):
         else:
             link_video = url_input.value
             page.clean()
-            page.add(ft.Text(f"Link do video: {link_video}"))
+            page.add(
+                ft.Text(f"Link do video: {link_video}")
+            )
 
     url_input = ft.TextField(label="URL youtube")
 
@@ -23,8 +35,17 @@ def main(page: ft.Page):
         padding=15,
         alignment=ft.alignment.center
         ),
-        url_input, 
-        ft.ElevatedButton("Enviar", on_click=submit_link)
+        url_input,
+        ft.Container(
+            ft.ElevatedButton(
+                "Enviar",
+                on_click=submit_link,
+                style=ft.ButtonStyle(
+                    padding=ft.padding.symmetric(10, 150)
+                )
+            ),
+            alignment=ft.alignment.center,
+        )
     )
 
 ft.app(target=main)
