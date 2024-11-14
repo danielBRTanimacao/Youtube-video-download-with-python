837f694cfeee95df232bcba0fc76ba5ed9629965
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-14 11:50:25 -0300

    updating object VideoHandler

--- mobile/tests/test_downloader.py
+++ mobile/tests/test_downloader.py
@@ -8,17 +8,31 @@ from mobile.utils.downloader import VideoHandler
 from mobile.utils.validators import url_validator
 
 
+LIST_VIDEOS = [
+    "https://youtu.be/ISKwApr2Zw0?si=5MXgrewRdaYqpjjr",
+    "https:///ISKwApr2Zw0?si=5MXgrewRdaYqpjjr"
+]
+
 class VideoHandlerTestCase(unittest.TestCase):
     def setUp(self):
-        _LIST_VIDEOS = [
-            "https://youtu.be/ISKwApr2Zw0?si=5MXgrewRdaYqpjjr"
-        ]
-        self.video_test = VideoHandler(_LIST_VIDEOS[0])
+        self.video_test = VideoHandler(LIST_VIDEOS[0])
         return super().setUp()
     
+    def test_property_get_title_return_a_str(self):
+        self.assertIsInstance(self.video_test.get_title, str)
+
+    def test_property_get_thumbnail_url_return_a_str(self):
+        self.assertIsInstance(self.video_test.get_thumbnail_url, str)
+
     def test_raise_error_if_url_value_is_not_str(self):
         with self.assertRaises(AssertionError):
             url_validator(1)
+
+    def test_raise_false_if_url_value_is_not_validated(self):
+        self.assertEqual(url_validator(LIST_VIDEOS[1]), False)
+
+    def test_raise_true_if_url_value_is_validated(self):
+        self.assertEqual(url_validator(LIST_VIDEOS[0]), True)
 
 
 if __name__ == '__main__':
--- mobile/utils/downloader.py
+++ mobile/utils/downloader.py
@@ -1,19 +1,27 @@
 from pytube import YouTube
+from pytube.exceptions import PytubeError
 
 class VideoHandler:
     def __init__(self, link: str):
-        self._yt = YouTube(link)
+        self.yt = YouTube(link)
 
-    def video_download(self):
-        ...
+    def video_download(self) -> str:
+        return "making download video"
 
-    def audio_download(self):
-        ...
+    def audio_download(self) -> str:
+        return "making download audio"
 
-    def playlist_download(self):
-        ...
+    def playlist_download(self) -> str:
+        return "downloading a playlist"
 
     @property
     def get_title(self) -> str:
-        return self._yt.title
+        try:
+            return self.yt.title
+        except PytubeError as error:
+            return f"titulo: Error ao buscar titulo\ndetalhe: Não foi possivel carregar o titulo do video\nerro: {error}"
+    
+    @property
+    def get_thumbnail_url(self) -> str:
+        return self.yt.thumbnail_url
     
