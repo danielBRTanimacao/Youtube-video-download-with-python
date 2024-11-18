d02651386d45b586b1832467c1b2856a25bfc888
Daniel T <93400508+danielBRTanimacao@users.noreply.github.com>
2024-11-18 13:48:53 -0300

    creating stream to download

--- mobile/tests/test_downloader.py
+++ mobile/tests/test_downloader.py
@@ -9,6 +9,7 @@ from mobile.utils.validators import url_validator
 
 
 LIST_VIDEOS = [
+    "https://youtu.be/qzOlJX3H--Q?si=6o735aGUGbN2-ZCZ",
     "https://youtu.be/ISKwApr2Zw0?si=5MXgrewRdaYqpjjr",
     "https:///ISKwApr2Zw0?si=5MXgrewRdaYqpjjr"
 ]
@@ -17,6 +18,11 @@ class VideoHandlerTestCase(unittest.TestCase):
     def setUp(self):
         self.video_test = VideoHandler(LIST_VIDEOS[0])
         return super().setUp()
+
+    def test_streams_raise_error_if_link_is_not_validated_or_acess(self):
+        self.video_test = VideoHandler(LIST_VIDEOS[2])
+        with self.assertRaises(Exception):
+            self.video_test.streams
     
     def test_property_get_title_return_a_str(self):
         self.assertIsInstance(self.video_test.get_title, str)
@@ -29,7 +35,7 @@ class VideoHandlerTestCase(unittest.TestCase):
             url_validator(1)
 
     def test_raise_false_if_url_value_is_not_validated(self):
-        self.assertEqual(url_validator(LIST_VIDEOS[1]), False)
+        self.assertEqual(url_validator(LIST_VIDEOS[2]), False)
 
     def test_raise_true_if_url_value_is_validated(self):
         self.assertEqual(url_validator(LIST_VIDEOS[0]), True)
--- mobile/utils/downloader.py
+++ mobile/utils/downloader.py
@@ -3,7 +3,10 @@ import re
 
 class VideoHandler:
     def __init__(self, link: str):
-        self.yt = link
+        self.__headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+        }
+        self._url = link
 
     def video_download(self) -> bool:
         return True
@@ -13,12 +16,24 @@ class VideoHandler:
 
     def playlist_download(self) -> bool:
         return True
+    
+    @property
+    def streams(self):
+        response = requests.get(self._url, headers=self.__headers)
+
+        if response.status_code != 200: raise Exception(f"Erro ao acessar o youtube. Code: {response.status_code}")
+        
+        video_data = re.search(r'"url":"(https.*?mime=video.*?)"', response.text)
+        if not video_data: raise Exception("Não foi possivel encontrar URL do video.")
+
+        video_stream_url = video_data.group(1).replace("\\u0026", "&")
+        return video_stream_url
 
     @property
     def get_title(self) -> str:
-        return self.yt
+        return self._url
     
     @property
     def get_thumbnail_url(self) -> str:
-        return self.yt
+        return self._url
     
